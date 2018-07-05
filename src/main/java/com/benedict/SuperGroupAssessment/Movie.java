2bf2368fc0527f000bf0ea028df792257cb38fe3
Mufaro Benedict Pabazhira <bmmuffy@gmail.com>
2018-07-05 02:23:17 +0200

    JWT tockens working MYSQL connection Working screenshots there

--- src/main/java/com/benedict/SuperGroupAssessment/Movie.java
+++ src/main/java/com/benedict/SuperGroupAssessment/Movie.java
@@ -1,10 +1,30 @@
 package com.benedict.SuperGroupAssessment;
 
-public class Movie {
+import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name = "movies")
+@EntityListeners(AuditingEntityListener.class)
+
+public class Movie implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@NotBlank
 	private String name;
+	@NotBlank
 	private String genre;
+	@NotBlank
 	private String director;
 
 	public Long getId() {
